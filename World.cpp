be7b2bc7db629fb6aefc47c7fe6ccf684b9e94cc
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-09-14 03:17:41 +0300

    сщздали простейшие функий вывода

---
+++ World.cpp
@@ -0,0 +1,7 @@
+/*Yakubovskaya Anastasya st130155@student.spbu.ru Zadacha1*/
+
+#include <iostream>
+int main (int argc,char  *argv[])
+{
+        std::cout<<"Hello,world"<<std::endl;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string vvod;
+	std:: cin>>vvod;
+	std::cout<<"Hello," << vvod << std::endl;
+}
