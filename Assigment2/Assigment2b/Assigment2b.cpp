bcc34ea2ce02b1933d9959d756b17394105c3c4a
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-10-06 01:43:07 +0300

    wrote the code in one file

--- Assigment2/Assigment2b/Assigment2b.cpp
+++ Assigment2/Assigment2b/Assigment2b.cpp
@@ -1,16 +1,45 @@
-/*Yakubovskaya Anastasya st130155@student.spbu.ru Zadacha2b*/
 #include <iostream>
-#include <stack>
+#include<stack>
 #include<string>
-extern string name;
+#include<sstream>
+#include<cstring>
+
+
+double  Calculator (const char* input){
+    std::stack<double> stack;
+    char* simvol = strtok(const_cast<char*>(input)," ");
+    while (simvol != nullptr) {
+        if (std::strchr("0123456789", simvol[0])) {
+         stack.push(std::stod(simvol));
+        } else {
+
+            double a = stack.top(); stack.pop();
+            double b = stack.top(); stack.pop();
+
+            if (strcmp(simvol, "+") == 0) {
+                stack.push(a + b);
+            } else if (strcmp(simvol, "-") == 0) {
+                stack.push(a - b);
+            } else if (strcmp(simvol, "*") == 0) {
+                stack.push(a * b);
+            } else if (strcmp(simvol, "/") == 0) {
+                stack.push(a / b);
+            }
+        }
+        simvol = strtok(nullptr, " ");
+    }
+    return  stack.top();
+}
 
 int main() {
     std::string input;
     std::getline(std::cin, input);
 
     
-    double result = Calculator(input);
+    double result = Calculator(input.c_str());
     std::cout << result << std::endl;
 
     return 0;
 }
+
+
