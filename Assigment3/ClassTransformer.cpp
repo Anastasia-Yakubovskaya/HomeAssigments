eb772dba8e92a7a822cbe8b88f85229e3ddaf577
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-10-20 19:23:42 +0300

    add setter and getter, associations

--- Assigment3/ClassTransformer.cpp
+++ Assigment3/ClassTransformer.cpp
@@ -1,67 +1,86 @@
 #include <iostream>
 #include <string>
 
+class Pilot {
+private:
+    std::string name;
+public:
+    Pilot(std::string name) : name(name) {}
+    std::string getName() const { return name; }
+    void setName(std::string newName) { name = newName; }
+};
 
-class Transformer 
-{
+class Transformer {
 private:
     std::string name;
     std::string model;
     int power;
+    Pilot* pilot;
 public:
-    Transformer(std::string p_name, std::string p_model, int p_power) {
+    Transformer(std::string p_name, std::string p_model, int p_power, Pilot* pilot) {
         this->name = p_name;
         this->model = p_model;
         this->power = p_power;
+        this->pilot = pilot;
     }
-    ~Transformer () {
-        std::cout << name << " is being destroyd." << std::endl;
+    ~Transformer() {
+        std::cout << name << " is being destroyed." << std::endl;
     }
-    std::string GetDescription() { return name; }
-    //void setPilot (Pilot* newPilot) {pilot = newPilot;} 
+    std::string getName() const { return name; }
+    void setName(std::string newName) { name = newName; }
+
+    std::string getModel() const { return model; }
+    void setModel(std::string newModel) { model = newModel; }
+
+    int getPower() const { return power; }
+    void setPower(int newPower) { power = newPower; }
 };
 
-
-class SuperSila1 : public Transformer
-{
+class SuperSila1 : public Transformer {
 private:
     int speed;
 public:
-    SuperSila1(std:: string name, std::string model, int power, int speed) : Transformer(name, model, power) {
+    SuperSila1(std::string name, std::string model, int power, Pilot* pilot, int speed) : Transformer(name, model, power, pilot) {
         this->speed = speed;
-    };
-    int getSpeed() { return speed;}
+    }
+    int getSpeed() const { return speed; }
+    void setSpeed(int newSpeed) { speed = newSpeed; }
 };
-
 
 class SuperSila2 : public Transformer {
 private:
-    int Timeoftransformation;
+    int timeOfTransformation; 
 public:
-    SuperSila2(std:: string name, std::string model, int power, int Timeoftransformation) : Transformer (name,model,power) {
-        this -> Timeoftransformation = Timeoftransformation;
+    SuperSila2(std::string name, std::string model, int power, Pilot* pilot, int timeOfTransformation) : Transformer(name, model, power, pilot) {
+        this->timeOfTransformation = timeOfTransformation;
     }
-    int getTimeoftransformation() { return Timeoftransformation; }
+    int getTimeOfTransformation() const { return timeOfTransformation; }
+    void setTimeOfTransformation(int newTimeOfTransformation) { timeOfTransformation = newTimeOfTransformation; }
 };
 
-
-class Tankcapacity : public Transformer {
+class TankCapacity : public Transformer {
 private:
     int volume;
 public:
-    Tankcapacity (std:: string name, std:: string model, int power, int volume) : Transformer (name,model,power) {
+    TankCapacity(std::string name, std::string model, int power, Pilot* pilot, int volume) : Transformer(name, model, power, pilot) {
         this->volume = volume;
     }
-    int getVolume() { return volume; }
+    int getVolume() const { return volume; }
+    void setVolume(int newVolume) { volume = newVolume; }
 };
-int main () {
-    SuperSila1 optimusprime("Optimus Prime", "AO-1", 1000, 60);
-    SuperSila2 megatron("Megatron", "DM-2", 1200, 80);
-    Tankcapacity bumblebee("Bumblebee", "VW-1", 700, 60);
 
-    std::cout << optimusprime.GetDescription() << std::endl;
-    std::cout << megatron.GetDescription() << std::endl;
-    std::cout << bumblebee.GetDescription() << std::endl;
+int main() {
+    Pilot pilot1("Pilot1");
+    Pilot pilot2("Pilot2");
+
+    SuperSila1 optimusprime("Optimus Prime", "fff", 1000, &pilot1, 60);
+    SuperSila2 megatron("Megatron", "eee", 1200, &pilot2, 80);
+    TankCapacity bumblebee("Bumblebee", "Vfff", 700, &pilot1, 60); 
+
+   
+    std::cout << optimusprime.getName()  << optimusprime.getSpeed() << std::endl;
+    std::cout << megatron.getName() << megatron.getTimeOfTransformation() << std::endl;
+    std::cout << bumblebee.getName() << bumblebee.getVolume() << std::endl;
 
     return 0;
 }
