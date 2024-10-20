2b5e659fbb4ce7a692e02924d19855aa61205c88
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-10-20 20:50:26 +0300

    add compositions and weapon class

--- Assigment3/ClassTransformer.cpp
+++ Assigment3/ClassTransformer.cpp
@@ -6,8 +6,26 @@ private:
     std::string name;
 public:
     Pilot(std::string name) : name(name) {}
-    std::string getName() const { return name; }
-    void setName(std::string newName) { name = newName; }
+    std::string getName() const {
+        return name;
+    }
+    void setName(std::string newName) {
+        name = newName;
+    }
+};
+
+class Weapon {
+private:
+    std::string type;
+    int damage;
+public:
+    Weapon(std::string type, int damage) : type(type), damage(damage) {}
+    std::string getType() const {
+        return type;
+    }
+    int getDamage() const {
+        return damage;
+    }
 };
 
 class Transformer {
@@ -15,17 +33,16 @@ private:
     std::string name;
     std::string model;
     int power;
-    Pilot* pilot;
+    Pilot& pilot; // ассоциация
+    Weapon weapon; // композиция
 public:
-    Transformer(std::string p_name, std::string p_model, int p_power, Pilot* pilot) {
-        this->name = p_name;
-        this->model = p_model;
-        this->power = p_power;
-        this->pilot = pilot;
-    }
+    Transformer(std::string p_name, std::string p_model, int p_power, Pilot& pilot, Weapon weapon) 
+        : name(p_name), model(p_model), power(p_power), pilot(pilot), weapon(weapon) {}
+    
     ~Transformer() {
         std::cout << name << " is being destroyed." << std::endl;
     }
+    
     std::string getName() const { return name; }
     void setName(std::string newName) { name = newName; }
 
@@ -34,15 +51,22 @@ public:
 
     int getPower() const { return power; }
     void setPower(int newPower) { power = newPower; }
+
+    std::string getWeaponType() const {
+        return weapon.getType();
+    }
+    int getWeaponDamage() const {
+        return weapon.getDamage();
+    }
 };
 
 class SuperSila1 : public Transformer {
 private:
     int speed;
 public:
-    SuperSila1(std::string name, std::string model, int power, Pilot* pilot, int speed) : Transformer(name, model, power, pilot) {
-        this->speed = speed;
-    }
+    SuperSila1(std::string name, std::string model, int power, Pilot& pilot, Weapon weapon, int speed)
+        : Transformer(name, model, power, pilot, weapon), speed(speed) {}
+
     int getSpeed() const { return speed; }
     void setSpeed(int newSpeed) { speed = newSpeed; }
 };
@@ -51,9 +75,9 @@ class SuperSila2 : public Transformer {
 private:
     int timeOfTransformation; 
 public:
-    SuperSila2(std::string name, std::string model, int power, Pilot* pilot, int timeOfTransformation) : Transformer(name, model, power, pilot) {
-        this->timeOfTransformation = timeOfTransformation;
-    }
+    SuperSila2(std::string name, std::string model, int power, Pilot& pilot, Weapon weapon, int timeOfTransformation)
+        : Transformer(name, model, power, pilot, weapon), timeOfTransformation(timeOfTransformation) {}
+
     int getTimeOfTransformation() const { return timeOfTransformation; }
     void setTimeOfTransformation(int newTimeOfTransformation) { timeOfTransformation = newTimeOfTransformation; }
 };
@@ -62,25 +86,36 @@ class TankCapacity : public Transformer {
 private:
     int volume;
 public:
-    TankCapacity(std::string name, std::string model, int power, Pilot* pilot, int volume) : Transformer(name, model, power, pilot) {
-        this->volume = volume;
-    }
+    TankCapacity(std::string name, std::string model, int power, Pilot& pilot, Weapon weapon, int volume)
+        : Transformer(name, model, power, pilot, weapon), volume(volume) {}
+
     int getVolume() const { return volume; }
     void setVolume(int newVolume) { volume = newVolume; }
 };
 
 int main() {
-    Pilot pilot1("Pilot1");
-    Pilot pilot2("Pilot2");
+    Pilot pilot1("Optimus Prime");
+    Pilot pilot2("Megatron");
 
-    SuperSila1 optimusprime("Optimus Prime", "fff", 1000, &pilot1, 60);
-    SuperSila2 megatron("Megatron", "eee", 1200, &pilot2, 80);
-    TankCapacity bumblebee("Bumblebee", "Vfff", 700, &pilot1, 60); 
+    Weapon weapon1("pistolet", 101010);
+    Weapon weapon2("blaster", 455);
+    Weapon weapon3("laser", 6967);
 
-   
-    std::cout << optimusprime.getName()  << optimusprime.getSpeed() << std::endl;
-    std::cout << megatron.getName() << megatron.getTimeOfTransformation() << std::endl;
-    std::cout << bumblebee.getName() << bumblebee.getVolume() << std::endl;
+    SuperSila1 optimusprime("Optimus Prime", "AO-1", 1000, pilot1, weapon1, 60);
+    SuperSila2 megatron("Megatron", "DM-2", 1200, pilot2, weapon2, 80); 
+    TankCapacity bumblebee("Bumblebee", "VW-1", 700, pilot1, weapon3, 60); 
 
+    std::cout << optimusprime.getName() << optimusprime.getSpeed() 
+              << optimusprime.getWeaponType() 
+              << optimusprime.getWeaponDamage() << std::endl;
+
+    std::cout << megatron.getName() << megatron.getTimeOfTransformation() 
+              <<  megatron.getWeaponType() 
+              <<  megatron.getWeaponDamage() << std::endl;
+
+    std::cout << bumblebee.getName() << bumblebee.getVolume() 
+              << bumblebee.getWeaponType() 
+              <<  bumblebee.getWeaponDamage() << std::endl;
+    
     return 0;
 }
