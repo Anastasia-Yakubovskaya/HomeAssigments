ee8dcf1e9f5da014e74a1d2fd23af8486eab74f5
Anastasia-Yakubovskaya <st130155@student.spbu.ru>
2024-11-09 21:37:48 +0300

    add a testtransformer2

--- Assigment4/test-supersila2.cpp
+++ Assigment4/test-supersila2.cpp
@@ -3,19 +3,118 @@
 #include "SuperSila2.h"
 #include "Pilot.h"
 #include "Weapon.h"
-#include "Transformer.h"
 
-TEST(SuperSila2, getTimeOfTransformation) {
+TEST(SuperSila2, Initialization) {
     Pilot pilot("Optimus Prime"); 
     Weapon weapon("Blaster", 100); 
-    SuperSila2 supersila2("Optimus", "Model X", 1000, pilot, weapon, 10); 
-    EXPECT_EQ(supersila2.getTimeOfTransformation(), 10); 
+    SuperSila2 supersila2("Optimus", "Model X", 1000, pilot, weapon, 20); 
+    EXPECT_EQ(supersila2.getTimeOfTransformation(), 20); 
 }
 
-TEST(SuperSila2, setTimeOfTransformation) {
+
+TEST(SuperSila2, ModifyTimeOfTransformation) {
+    Pilot pilot("Optimus Prime"); 
+    Weapon weapon("Blaster", 100); 
+    SuperSila2 supersila2("Optimus", "Model X", 1000, pilot, weapon, 30); 
+    supersila2.setTimeOfTransformation(25); 
+    EXPECT_EQ(supersila2.getTimeOfTransformation(), 25); 
+}
+
+
+TEST(SuperSila2, OperatorLessThan) {
+    Pilot pilot1("Optimus Prime"); 
+    Weapon weapon1("Blaster", 100); 
+    SuperSila2 supersila1("Optimus", "Model X", 1000, pilot1, weapon1, 15); 
+    
+    Pilot pilot2("Megatron"); 
+    Weapon weapon2("Cannon", 150); 
+    SuperSila2 supersila2("Megatron", "Model Y", 600, pilot2, weapon2, 25); 
+
+    EXPECT_TRUE(supersila1 < supersila2);  
+}
+
+
+TEST(SuperSila2, OperatorGreaterThan) {
+    Pilot pilot1("Optimus Prime"); 
+    Weapon weapon1("Blaster", 100); 
+    SuperSila2 supersila1("Optimus", "Model X", 1000, pilot1, weapon1, 30); 
+    
+    Pilot pilot2("Megatron"); 
+    Weapon weapon2("Cannon", 150); 
+    SuperSila2 supersila2("Megatron", "Model Y", 600, pilot2, weapon2, 20); 
+
+    EXPECT_TRUE(supersila1 > supersila2); 
+}
+
+
+TEST(SuperSila2, OperatorEquals) {
+    Pilot pilot1("Optimus Prime"); 
+    Weapon weapon1("Blaster", 100); 
+    SuperSila2 supersila1("Optimus", "Model X", 1000, pilot1, weapon1, 15); 
+    
+    Pilot pilot2("Megatron"); 
+    Weapon weapon2("Cannon", 150); 
+    SuperSila2 supersila2("Megatron", "Model Y", 600, pilot2, weapon2, 15); 
+
+    EXPECT_TRUE(supersila1 == supersila2);  
+}
+
+TEST(SuperSila2, GetTimeOfTransformation) {
     Pilot pilot("Optimus Prime");
     Weapon weapon("Blaster", 100);
-    SuperSila2 supersila2("Optimus", "Model X", 1000, pilot, weapon, 150); 
-    supersila2.setTimeOfTransformation(15); 
-    EXPECT_EQ(supersila2.getTimeOfTransformation(), 15); 
+    SuperSila2 supersila2("Optimus", "Model X", 1000, pilot, weapon, 10);
+
+   
+    EXPECT_EQ(supersila2.getTimeOfTransformation(), 10);
+}
+
+
+TEST(SuperSila2, SetTimeOfTransformation) {
+    Pilot pilot("Optimus Prime");
+    Weapon weapon("Blaster", 100);
+    SuperSila2 supersila2("Optimus", "Model X", 1000, pilot, weapon, 20); 
+
+
+    supersila2.setTimeOfTransformation(15);
+
+    EXPECT_EQ(supersila2.getTimeOfTransformation(), 15);
+}
+
+
+TEST(SuperSila2, OperatorNotEquals) {
+    Pilot pilot1("Optimus Prime"); 
+    Weapon weapon1("Blaster", 100); 
+    SuperSila2 supersila1("Optimus", "Model X", 1000, pilot1, weapon1, 15); 
+    
+    Pilot pilot2("Megatron"); 
+    Weapon weapon2("Cannon", 150); 
+    SuperSila2 supersila2("Megatron", "Model Y", 600, pilot2, weapon2, 20); 
+
+    EXPECT_TRUE(supersila1 != supersila2);  
+}
+
+
+TEST(SuperSila2, OperatorLessThanOrEqual) {
+    Pilot pilot1("Optimus Prime"); 
+    Weapon weapon1("Blaster", 100); 
+    SuperSila2 supersila1("Optimus", "Model X", 1000, pilot1, weapon1, 20); 
+    
+    Pilot pilot2("Megatron"); 
+    Weapon weapon2("Cannon", 150); 
+    SuperSila2 supersila2("Megatron", "Model Y", 600, pilot2, weapon2, 25); 
+
+    EXPECT_TRUE(supersila1 <= supersila2); 
+}
+
+
+TEST(SuperSila2, OperatorGreaterThanOrEqual) {
+    Pilot pilot1("Optimus Prime"); 
+    Weapon weapon1("Blaster", 100); 
+    SuperSila2 supersila1("Optimus", "Model X", 1000, pilot1, weapon1, 25); 
+    
+    Pilot pilot2("Megatron"); 
+    Weapon weapon2("Cannon", 150); 
+    SuperSila2 supersila2("Megatron", "Model Y", 600, pilot2, weapon2, 20); 
+
+    EXPECT_TRUE(supersila1 >= supersila2); 
 }
